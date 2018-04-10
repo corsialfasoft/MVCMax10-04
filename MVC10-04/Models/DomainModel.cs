57af2caa8c121fa5c3fc621dfd0043f7491fce77
emmeacs <massimo.franzoso8895@gmail.com>
2018-04-10 11:19:22 +0200

    Aggiunto SearchByDescription,SearchByID

--- MVC10-04/Models/DomainModel.cs
+++ MVC10-04/Models/DomainModel.cs
@@ -12,11 +12,55 @@ namespace MVC10_04.Models
 		void AddCarrello (Prodotto p,int quantità);
 		void Ordina(List<Ordine> ordini);
 	}
-	public partial class DomainModel
+	public partial class DomainModel : IDomainModel
 	{
+		public void AddCarrello(Prodotto p,int quantità)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Ordina(List<Ordine> ordini)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<Prodotto> SearchByDescription(string descrizione)
+		{
+			List <Prodotto> prodotti = new List<Prodotto>();
+			using (var db =new OrdiniEntities()) {
+				var query = from product in db.ProdottiSet 
+							where product.descrizione.Contains(descrizione)
+							select product;
+				foreach (var element in query) { 
+					Prodotto x = new Prodotto();
+					x.ID=element.Id;
+					x.Descrizione=element.descrizione;
+					prodotti.Add(x);
+					}
+				return prodotti;
+ 			}
+		}
+
+		public Prodotto SearchById(int id)
+		{
+			using ( OrdiniEntities db = new OrdiniEntities()) {
+				var ricerca = db.ProdottiSet.Find(id);
+				Prodotto trovato = new Prodotto();
+				if (ricerca != null) {
+					trovato.ID = ricerca.Id;
+					trovato.Descrizione = ricerca.descrizione;
+				}
+				return trovato;
+			}
+		}
 	}
 	public class Prodotto
 	{
+		private int _id;
+		public int ID {
+			get { return _id;}
+			set { _id=value;}
+		}
 		private string _descrizione;
 		public string Descrizione {
 			get {return _descrizione;}
